5eb3ecd02c2627535d0925f16818b943d6a4f9cd
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2021-04-20 23:48:57 +0200

    more test cases

--- Editor/BFN_Tests.cs
+++ Editor/BFN_Tests.cs
@@ -18,11 +18,21 @@ public class BFN_Tests
     [Test] public void addition_1 () => Assert.AreEqual( expected:new BFN(12,3) , actual:new BFN(1,4) + new BFN(2,3) );
     [Test] public void addition_2 () => Assert.AreEqual( expected:new BFN(9.87654321,33) , actual:new BFN(9.87654321,33) + new BFN(1.23456789,14) );
     [Test] public void addition_3 () => Assert.AreEqual( expected:new BFN(30.000004,9) , actual:new BFN(3,10) + new BFN(4,3) );
-    [Test] public void subtraction_1 () => Assert.AreEqual( expected:new BFN(29.999996,9) , actual:new BFN(3,10) - new BFN(4,3) );
+    [Test] public void addition_4 () => Assert.AreEqual( expected:new BFN(1,14) , actual:new BFN(1,14) + new BFN(0,13) );
+    [Test] public void addition_5 () => Assert.AreEqual( expected:new BFN(1,14) , actual:new BFN(1,14) + new BFN(0,-13) );
+    [Test] public void addition_6 () => Assert.AreEqual( expected:new BFN(-1,-14) , actual:new BFN(-1,-14) + new BFN(0,-13) );
+
+    [Test] public void subtraction_1 () => Assert.AreEqual( expected:new BFN(0,0) , actual:new BFN(1,0) - new BFN(1,0) );
+    [Test] public void subtraction_2 () => Assert.AreEqual( expected:new BFN(-1,0) , actual:new BFN(1,0) - new BFN(2,0) );
+    [Test] public void subtraction_3 () => Assert.AreEqual( expected:new BFN(29.999996,9) , actual:new BFN(3,10) - new BFN(4,3) );
+    [Test] public void subtraction_4 () => Assert.AreEqual( expected:new BFN(1,100) , actual:new BFN(1,100) - new BFN(1,70) );
+    [Test] public void subtraction_5 () => Assert.AreEqual( expected:new BFN(1,10) , actual:new BFN(1,10) - new BFN(0,10) );
     
     [Test] public void multiplication_1 () => Assert.AreEqual( expected:new BFN(600,0).compressed , actual:new BFN(2,1) * new BFN(3,1) );
     [Test] public void multiplication_2 () => Assert.AreEqual( expected:new BFN(1.2,14).compressed , actual:new BFN(3,10) * new BFN(4,3) );
     
-    [Test] public void division_1 () => Assert.AreEqual( expected:new BFN(7.5,6) , actual:new BFN(3,10) / new BFN(4,3) );
+    [Test] public void division_1 () => Assert.AreEqual( expected:new BFN(500,0) , actual:new BFN(1,3) / 2 );
+    [Test] public void division_2 () => Assert.AreEqual( expected:new BFN(50,0) , actual:new BFN(1,3) / new BFN(2,1) );
+    [Test] public void division_3 () => Assert.AreEqual( expected:new BFN(7.5,6) , actual:new BFN(3,10) / new BFN(4,3) );
 
 }
