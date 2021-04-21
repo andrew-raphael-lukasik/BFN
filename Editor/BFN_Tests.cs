537c583c1af140ece4ef0126d82cf40cf0123a1f
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2021-04-21 17:04:45 +0200

    naming change

--- Editor/BFN_Tests.cs
+++ Editor/BFN_Tests.cs
@@ -5,34 +5,34 @@ using UnityEngine.TestTools;
 public class BFN_Tests
 {
 
-	[Test] public void equality_between_2_defaults () => Assert.AreEqual( expected: default(BFN) , actual: default(BFN) );
-	[Test] public void equality_between_default_and_simplified_default () => Assert.AreEqual( expected: default(BFN) , actual: default(BFN).compressed );
-
-    [Test] public void equality_between_value_and_its_simplified_value_1 () => Assert.AreEqual( expected:new BFN(1,0) , actual:new BFN(1,0).compressed );
-    [Test] public void equality_between_value_and_its_simplified_value_2 () => Assert.AreEqual( expected:new BFN(1,3) , actual:new BFN(1000,0).compressed );
-
-    [Test] public void simplification_positive_exponent_1 () => Assert.AreEqual( expected:new BFN(12.345678910111213141,18) , actual:new BFN(12345678910111213141,0).compressed );
-    [Test] public void simplification_positive_exponent_2 () => Assert.AreEqual( expected:new BFN(9.999999999999999999,18) , actual:new BFN(9999999999999999999,0).compressed );
-    [Test] public void simplification_negative_exponent_1 () => Assert.AreEqual( expected:new BFN(0.01,-3) , actual:new BFN(0.00001,0).compressed );
+	[Test] public void equal__default_default () => Assert.AreEqual( expected: default(BFN) , actual: default(BFN) );
+	[Test] public void equal__default_default_compressed () => Assert.AreEqual( expected: default(BFN) , actual: default(BFN).compressed );
+    [Test] public void equal__1e0_1e0_compressed () => Assert.AreEqual( expected:new BFN(1,0) , actual:new BFN(1,0).compressed );
     
-    [Test] public void addition_1 () => Assert.AreEqual( expected:new BFN(12,3) , actual:new BFN(1,4) + new BFN(2,3) );
-    [Test] public void addition_2 () => Assert.AreEqual( expected:new BFN(9.87654321,33) , actual:new BFN(9.87654321,33) + new BFN(1.23456789,14) );
-    [Test] public void addition_3 () => Assert.AreEqual( expected:new BFN(30.000004,9) , actual:new BFN(3,10) + new BFN(4,3) );
-    [Test] public void addition_4 () => Assert.AreEqual( expected:new BFN(1,14) , actual:new BFN(1,14) + new BFN(0,13) );
-    [Test] public void addition_5 () => Assert.AreEqual( expected:new BFN(1,14) , actual:new BFN(1,14) + new BFN(0,-13) );
-    [Test] public void addition_6 () => Assert.AreEqual( expected:new BFN(-1,-14) , actual:new BFN(-1,-14) + new BFN(0,-13) );
+    [Test] public void non_equal__1e3_1e3_compressed () => Assert.AreNotEqual( expected:new BFN(1,3) , actual:new BFN(1000,0) );
 
-    [Test] public void subtraction_1 () => Assert.AreEqual( expected:new BFN(0,0) , actual:new BFN(1,0) - new BFN(1,0) );
-    [Test] public void subtraction_2 () => Assert.AreEqual( expected:new BFN(-1,0) , actual:new BFN(1,0) - new BFN(2,0) );
-    [Test] public void subtraction_3 () => Assert.AreEqual( expected:new BFN(29.999996,9) , actual:new BFN(3,10) - new BFN(4,3) );
-    [Test] public void subtraction_4 () => Assert.AreEqual( expected:new BFN(1,100) , actual:new BFN(1,100) - new BFN(1,70) );
-    [Test] public void subtraction_5 () => Assert.AreEqual( expected:new BFN(1,10) , actual:new BFN(1,10) - new BFN(0,10) );
+    [Test] public void compress__positive_exponent_1 () => Assert.AreEqual( expected:new BFN(12.345678910111213141,18) , actual:new BFN(12345678910111213141,0).compressed );
+    [Test] public void compress__positive_exponent_2 () => Assert.AreEqual( expected:new BFN(9.999999999999999999,18) , actual:new BFN(9999999999999999999,0).compressed );
+    [Test] public void compress__negative_exponent_1 () => Assert.AreEqual( expected:new BFN(0.01,-3) , actual:new BFN(0.00001,0).compressed );
     
-    [Test] public void multiplication_1 () => Assert.AreEqual( expected:new BFN(600,0).compressed , actual:new BFN(2,1) * new BFN(3,1) );
-    [Test] public void multiplication_2 () => Assert.AreEqual( expected:new BFN(1.2,14).compressed , actual:new BFN(3,10) * new BFN(4,3) );
+    [Test] public void addition__1e4_2e3 () => Assert.AreEqual( expected:new BFN(12,3) , actual:new BFN(1,4) + new BFN(2,3) );
+    [Test] public void addition__9dot87654321e33_1dot23456789e14 () => Assert.AreEqual( expected:new BFN(9.87654321,33) , actual:new BFN(9.87654321,33) + new BFN(1.23456789,14) );
+    [Test] public void addition__3e10_4e3 () => Assert.AreEqual( expected:new BFN(30.000004,9) , actual:new BFN(3,10) + new BFN(4,3) );
+    [Test] public void addition__1e14_0e13 () => Assert.AreEqual( expected:new BFN(1,14) , actual:new BFN(1,14) + new BFN(0,13) );
+    [Test] public void addition__1e14_0en13 () => Assert.AreEqual( expected:new BFN(1,14) , actual:new BFN(1,14) + new BFN(0,-13) );
+    [Test] public void addition__n1en14_0en13 () => Assert.AreEqual( expected:new BFN(-1,-14) , actual:new BFN(-1,-14) + new BFN(0,-13) );
+
+    [Test] public void subtract__1e0_1e0 () => Assert.AreEqual( expected:new BFN(0,0) , actual:new BFN(1,0) - new BFN(1,0) );
+    [Test] public void subtract__1e0_2e0 () => Assert.AreEqual( expected:new BFN(-1,0) , actual:new BFN(1,0) - new BFN(2,0) );
+    [Test] public void subtract__3e10_4e3 () => Assert.AreEqual( expected:new BFN(29.999996,9) , actual:new BFN(3,10) - new BFN(4,3) );
+    [Test] public void subtract__1e100_1e70 () => Assert.AreEqual( expected:new BFN(1,100) , actual:new BFN(1,100) - new BFN(1,70) );
+    [Test] public void subtract__1e10_0e10 () => Assert.AreEqual( expected:new BFN(1,10) , actual:new BFN(1,10) - new BFN(0,10) );
     
-    [Test] public void division_1 () => Assert.AreEqual( expected:new BFN(500,0) , actual:new BFN(1,3) / 2 );
-    [Test] public void division_2 () => Assert.AreEqual( expected:new BFN(50,0) , actual:new BFN(1,3) / new BFN(2,1) );
-    [Test] public void division_3 () => Assert.AreEqual( expected:new BFN(7.5,6) , actual:new BFN(3,10) / new BFN(4,3) );
+    [Test] public void multiply__2e1_3e1 () => Assert.AreEqual( expected:new BFN(600,0).compressed , actual:new BFN(2,1) * new BFN(3,1) );
+    [Test] public void multiply__3e10_4e3 () => Assert.AreEqual( expected:new BFN(1.2,14).compressed , actual:new BFN(3,10) * new BFN(4,3) );
+    
+    [Test] public void divide__1e3_2 () => Assert.AreEqual( expected:new BFN(500,0) , actual:new BFN(1,3) / 2 );
+    [Test] public void divide__1e3_2e1 () => Assert.AreEqual( expected:new BFN(50,0) , actual:new BFN(1,3) / new BFN(2,1) );
+    [Test] public void divide__3e10_4e3 () => Assert.AreEqual( expected:new BFN(7.5,6) , actual:new BFN(3,10) / new BFN(4,3) );
 
 }
