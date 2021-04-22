1c70a1993a45b406047c181fab4f933721a24fe2
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2021-04-22 23:08:55 +0200

    no message

--- BFN_ExampleComponent.cs
+++ BFN_ExampleComponent.cs
@@ -2,9 +2,30 @@
 using UnityEngine;
 public class BFN_ExampleComponent : MonoBehaviour
 {
-	[SerializeField] BFN _myBigNumber = new BFN{ number=11.2423085208 , exponent=21 };
+	[SerializeField] BFN _a = new BFN{ number=11.2423085208 , exponent=21 };
+	[SerializeField] BFN _b = new BFN{ number=12312425.14124312 , exponent=100 };
+	[SerializeField] OP _operator = OP.Add;
+	[SerializeField] BFN _result = (BFN) 0;
+
+	public enum OP : byte { Add , Subtract , Multiply , Divide }
+
+	#if UNITY_EDITOR
+	void OnValidate ()
+	{
+		switch( _operator )
+		{
+			case OP.Add:		_result = _a + _b; break;
+			case OP.Subtract:	_result = _a - _b; break;
+			case OP.Multiply:	_result = _a * _b; break;
+			case OP.Divide:		_result = _a / _b; break;
+			default: throw new System.NotImplementedException();
+		}
+	}
+	#endif
+	
 	void Start ()
 	{
-		Debug.Log($"my number is: {_myBigNumber}");
+		Debug.Log($"{_a} {_operator} {_b} = {_result}");
 	}
+
 }
